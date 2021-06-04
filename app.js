5b973d164f27b51cb18d8955ec479ae9a820eb3e
Syed Khalil <83117351+SyedKhalil51@users.noreply.github.com>
2021-06-04 19:21:50 +0500

    Js Coding Task 4

---
+++ app.js
@@ -0,0 +1,47 @@
+// Part A: 1. Problem Name: ForLoop A. Create a for loop. 
+
+for(var i = 0; i<50; i++)
+{
+  console.log(i +" clap");
+}
+
+// Part B: 1. Problem Name: ForLoop A. Create a for loop.
+
+for(var a = 0; a<200; a++)
+{
+  console.log(a);
+}
+
+//  2. Problem Name: ForLoopWithDecrementOperator 
+
+
+/* for (var x =200; x> 0; x-3)
+ {
+
+  console.log(x);
+
+} */
+
+//3. Problem Name: InfiniteForLoop
+/*for(; ;) it willl create infinite loop
+{
+
+}
+*/ 
+//4. Problem Name: WhileLoop
+
+var z = 0;
+while(z<=10)
+{
+  console.log(z +"  Syed Khalil");
+  z++;
+}
+
+//5. Problem Name: Do-WhileLoop
+
+var y = 0;
+do{
+  console.log(y +"  Syed Khalil");
+  y++;
+}
+while(y<=10);
